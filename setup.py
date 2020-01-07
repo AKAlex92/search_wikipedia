fd8d4f909601306405891850bb5e1aa23cff39e1
AKAlex92 <pi@alessandroraspberry.it>
2020-01-07 10:27:46 +0100

    .

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setup(
 	install_requires     = requires,
 	packages             = find_packages(),
 	include_package_data=True,
-	entry_points         = {'console_scripts': ['search_wiki = search_wikipedia.__main__:main']}
+	entry_points         = {'console_scripts': ['search_wiki = search_wiki:main']}
 	)
